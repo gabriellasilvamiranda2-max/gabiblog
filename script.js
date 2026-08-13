dbe1b84a7a71d48208c85e9d575157f71928ae20
gabriellasilvamiranda2-max <gabriellasilvamiranda2@gmail.com>
2026-08-13 09:53:30 -0300

    cor

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 const botoes = document.querySelectorAll('button');
 
 botoes.forEach(botao => {
-    botao.addEventListener('click,'curtir)
-
+    botao.addEventListener('click', curtir)
+     let curtiu = false
     function curtir (){
     let curtidas = document.querySelector('span')
-    curtidas.textContent++
+
+    if (curtiu == false){
+        curtiu = true
+        curtidas.textContent++
+        botao.style.backgroundColor = 'blue'
+    }
+    
     }
 });
